d140b55c41cafe477379751ca61a6e6e18deabb1
t_vadymk <vadym.kovalchuk@citrix.com>
2020-02-20 23:26:51 +0200

    first blood

---
+++ agent/agent.py
@@ -0,0 +1,23 @@
+import zmq
+
+
+def main():
+    context = zmq.Context()
+
+    #  Socket to talk to server
+    print("Connecting to hello world server…")
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:9999")
+
+    #  Do 10 requests, waiting each time for a response
+    for request in range(10000):
+        print("Sending request %s …" % request)
+        socket.send(b"Hello")
+
+        #  Get the reply.
+        message = socket.recv()
+        print("Received reply %s [ %s ]" % (request, message))
+
+
+if __name__ == '__main__':
+    main()
---
+++ relay/relay.py
@@ -0,0 +1,44 @@
+import json
+import asyncio
+from collections import deque
+
+import zmq
+
+from lib.sockets import get_server_socket
+
+
+agents = []
+clients = []
+tasks = deque([])
+
+
+async def server_loop():
+    socket, poller = await get_server_socket('tcp://*', '9999')
+    while True:
+        sockets = dict(await poller.poll(1000))
+        print("waiting")
+        if sockets.get(socket, None) == zmq.POLLIN:
+            #  Wait for next request from client
+            #  await socket.recv_json()
+            message = await socket.recv()
+            print("Received request: %s" % message)
+            #  Do some 'work'
+            #  Send reply back to client
+            await socket.send(b"World")
+
+
+async def socket_out_loop():
+    await asyncio.sleep(1)
+
+
+async def rump_up():
+    _server = asyncio.create_task(server_loop())
+    await _server
+
+
+def main():
+    asyncio.run(rump_up())
+
+
+if __name__ == '__main__':
+    main()
