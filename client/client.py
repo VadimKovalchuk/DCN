b0d767cca61ba524aaf67b1bf428e721918858d7
VadimKovalchuk <kovalchuk.vadim@gmail.com>
2020-02-22 01:25:37 +0200

    Relay class in client

--- client/client.py
+++ client/client.py
@@ -3,12 +3,23 @@ import zmq
 context = zmq.Context()
 
 #  Socket to talk to server
-print("Connecting to hello world server…")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:9999")
+# print("Connecting to hello world server…")
+# socket = context.socket(zmq.REQ)
+# socket.connect("tcp://localhost:9999")
+
+
+class RelayHandler:
+    def __init__(self):
+        self.ip = 'localhost'
+        self.port = '9999'
+
+    def register(self):
+        print(f'Register on Relay {self.ip}:{self.port}')
+        socket = context.socket(zmq.REQ)
+        socket.connect(f'tcp://{self.ip}:{self.port}')
 
 #  Do 10 requests, waiting each time for a response
-for request in range(10000):
+for request in range(10):
     print("Sending request %s …" % request)
     socket.send(b"Hello")
 
---
+++ lib/request_types.py
@@ -0,0 +1,15 @@
+register = {
+    'command': 'register',
+    'type': None,
+    'name': 'undef',
+}
+
+pulse = {
+    'command': 'pulse',
+    'name': 'undef',
+}
+
+task = {}
+
+task_report = {}
+
