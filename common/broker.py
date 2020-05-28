d6536db33f236c0e65b7114d8b7c8fff2ad80f56
VadymKovalchuk <kovalchuk.vadim@gmail.com>
2020-05-28 23:24:16 +0300

    briker finalize

--- common/broker.py
+++ common/broker.py
@@ -1,14 +1,18 @@
+import json
 import logging
+from typing import Callable
+
 import pika
 
 logger = logging.getLogger(__name__)
-logging.getLogger("pika").setLevel(logging.WARNING)
+logging.getLogger('pika').setLevel(logging.WARNING)
 # RabbitMQ running is required for this module operations
-# docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management &
+# sudo docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management &
 
 
 class Broker:
     def __init__(self, host: str):
+        logger.info('Starting broker')
         self.host = host
         self.connection = None
         self.channel = None
@@ -23,35 +27,50 @@ class Broker:
         self.close()
 
     def connect(self):
+        logger.info('Broker connecting to server')
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host='localhost'))
         self.channel = self.connection.channel()
 
     def declare(self, input_queue: str = '', output_queue: str = ''):
-        self.input_queue = input_queue
-        self.output_queue = output_queue
-        logger.debug(f'in {self.input_queue}, out {self.output_queue}')
-        if not (self.input_queue or self.output_queue):
+        if not (input_queue or output_queue):
             raise RuntimeError('Queue is not defined for Broker')
-        for queue in [self.input_queue, self.output_queue]:
-            if queue:
-                self.channel.queue_declare(queue=queue)
+        if input_queue:
+            self.input_queue = input_queue
+            self.channel.queue_declare(queue=input_queue)
+        if output_queue:
+            self.output_queue = output_queue
+            self.channel.queue_declare(queue=output_queue)
 
-    def push(self, message):
-        # TODO: Validate out queue
-        logger.debug(f'sending: {message}')
-        self.channel.basic_publish(exchange='', routing_key=self.output_queue, body=message)
+    def push(self, message: dict):
+        if not self.output_queue:
+            raise RuntimeError('Trying to push results to queue '
+                               'that not defined')
+        msg_str = json.dumps(message, indent=4)
+        logger.debug(f'sending: {msg_str}')
+        self.channel.basic_publish(exchange='', routing_key=self.output_queue, body=msg_str)
 
-    def pull(self):
-        # TODO: Validate in queue
-        for method, properties, body in self.channel.consume(
+    def pull(self, processing_callback: Callable):
+        if not self.input_queue:
+            raise RuntimeError('Trying to pull task from queue '
+                               'that not defined')
+        for method, properties, tsk_str in self.channel.consume(
                 self.input_queue, inactivity_timeout=self._inactivity_timeout):
-            if not all((method, properties, body)):
+            if not all((method, properties, tsk_str)):
                 self.channel.cancel()
                 break
-            logger.debug(" [x] Received %r" % body)
+            logger.debug("Received: %r" % tsk_str)
+            try:
+                task = json.loads(tsk_str)
+            except TypeError:
+                logger.error(f'Invalid message content is received: {tsk_str}')
+                task = {}
+            reply: dict = processing_callback(method, properties, task)
+            if reply:
+                self.push(reply)
             self.channel.basic_ack(method.delivery_tag)
 
     def close(self):
+        logger.info('Closing broker connection')
         if self.connection:
             self.connection.close()
--- tests/test_broker_wrapper.py
+++ tests/test_broker_wrapper.py
@@ -1,25 +1,47 @@
 import logging
+from random import random
 
 from common.broker import Broker
 
 logger = logging.getLogger(__name__)
 
-test_queue = 'test'
+task_input_queue = 'test_task'
+task_result_queue = 'test_result'
+test_tasks = {i: {'id': i, 'task': random()} for i in range(10)}
+
+
+def processing_callback(method, properties, task):
+    if not all((method, properties, task)):
+        return False
+    else:
+        assert test_tasks[task['id']] == task, 'Task differs from expected one'
+        return {'id': task['id'], 'result': task['task']}
+
+
+def validator_callback(method, properties, task):
+    assert test_tasks[task['id']]['task'] == task['result'], 'Task differs from expected one'
+    return False
 
 
 def test_broker_smoke():
-    with Broker('localhost') as sender:
-        sender.connect()
-        sender.declare(output_queue=test_queue)
-        with Broker('localhost') as receiver:
-            receiver.connect()
-            receiver.declare(input_queue=test_queue)
-            receiver._inactivity_timeout = 0.1
-            # Exchange
-            logger.debug(sender.output_queue)
-            logger.debug(receiver.input_queue)
-            for _ in range(10):
-                sender.push(str(_))
-            receiver.pull()
+    with Broker('localhost') as client:
+        client.connect()
+        client.declare(output_queue=task_input_queue)
+        logger.info('Sending task')
+        for i in test_tasks:
+            client.push(test_tasks[i])
+    with Broker('localhost') as agent:
+        agent.connect()
+        agent.declare(input_queue=task_input_queue,
+                      output_queue=task_result_queue)
+        agent._inactivity_timeout = 0.1
+        logger.info('Processing task')
+        agent.pull(processing_callback)
+    with Broker('localhost') as validator:
+        validator.connect()
+        validator.declare(input_queue=task_result_queue)
+        validator._inactivity_timeout = 0.1
+        logger.info('Validating results')
+        validator.pull(validator_callback)
 
 
