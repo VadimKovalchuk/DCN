60128bdf2efd49bf7905f4a30fea5b8913809dd2
VadimKovalchuk <kovalchuk.vadim@gmail.com>
2022-11-08 23:39:12 +0200

    upd setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='dcn',
-    packages=find_packages(
-        include=['dcn']
-    ),
+    packages=find_packages(),
     install_requires=[
         'pika==1.3.1',
         'pyzmq==24.0.1'
